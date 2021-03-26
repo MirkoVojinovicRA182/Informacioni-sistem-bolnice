ace59e990bef5ba339d8ce0b9a5459dd6c3a1c4b
Mirko Vojinovic <vojinovic.ra182.2018@uns.ac.rs>
2021-03-26 13:58:47 +0100

    Kreiranje inicijalnih prozora

--- Project/HospitalInformationSystem/MainWindow.xaml.cs
+++ Project/HospitalInformationSystem/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using HospitalInformationSystem.Windows;
+
 namespace HospitalInformationSystem
 {
     /// <summary>
@@ -23,6 +25,26 @@ namespace HospitalInformationSystem
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void confirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            RoomManagementWindow roomManagementWindow = new RoomManagementWindow();
+            PatientAppointmentsManagementWindow patientAppManagementWindow = new PatientAppointmentsManagementWindow();
+            DoctorAppointmentsManagementWindow doctorAppManagementWindow = new DoctorAppointmentsManagementWindow();
+            PatientManagementWindow patientManagementWindow = new PatientManagementWindow();
+
+            if ((bool)roomRadioButton.IsChecked)
+                roomManagementWindow.Show();
+            else if ((bool)patientAppointmentsRadioButton.IsChecked)
+                patientAppManagementWindow.Show();
+            else if ((bool)doctorAppointmentsRadioButton.IsChecked)
+                doctorAppManagementWindow.Show();
+            else if ((bool)patientRadioButton.IsChecked)
+                patientAppManagementWindow.Show();
+            else
+                MessageBox.Show("Niste izabrali opciju!", "Greška", MessageBoxButton.OK);
+
         }
     }
 }

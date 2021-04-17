26c919d04210a915e81f4ceea8ee14959b17b83b
Mirko Vojinovic <vojinovic.ra182.2018@uns.ac.rs>
2021-04-17 22:52:09 +0200

    Adding new equipment through the form

---
+++ Project/HospitalInformationSystem/Controller/EquipmentController.cs
@@ -0,0 +1,24 @@
+﻿using HospitalInformationSystem.Service;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalInformationSystem.Controller
+{
+    class EquipmentController
+    {
+        EquipmentService equipmentService;
+        public EquipmentController()
+        {
+            equipmentService = new EquipmentService();
+        }
+
+        public void addNewEquipment(Equipment equipment)
+        {
+            equipmentService.addNewEquipment(equipment);
+        }
+    }
+}

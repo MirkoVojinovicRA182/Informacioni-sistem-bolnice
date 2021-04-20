9ba7aec366c2a0d341046f3e45868b095fbc10d2
SavDj <73643756+SavDj@users.noreply.github.com>
2021-04-21 01:00:44 +0200

    Bug fixing

--- Project/HospitalInformationSystem/Windows/NewPatientAppointmentSystemWindow.xaml.cs
+++ Project/HospitalInformationSystem/Windows/NewPatientAppointmentSystemWindow.xaml.cs
@@ -130,7 +130,7 @@ namespace HospitalInformationSystem.Windows
 
             for (int i = 0; i < dateTimes.Count; i++)
             {
-                recommendedAppointments.Add(new Appointment(dateTimes[i], TypeOfAppointment.Pregled, doctor.room, (Patient)patientComboBox.SelectedItem, doctor));
+                recommendedAppointments.Add(new Appointment(dateTimes[i], TypeOfAppointment.Pregled, doctor.room, (Model.Patient)patientComboBox.SelectedItem, doctor));
             }
 
             for (int i = 0; i < recommendedAppointments.Count; i++)
@@ -172,7 +172,7 @@ namespace HospitalInformationSystem.Windows
 
                     for (int i = 0; i < dateTimes.Count; i++)
                     {
-                        recommendedAppointments.Add(new Appointment(dateTimes[i], TypeOfAppointment.Pregled, doctor.room, (Patient)patientComboBox.SelectedItem, doctor));
+                        recommendedAppointments.Add(new Appointment(dateTimes[i], TypeOfAppointment.Pregled, doctor.room, (Model.Patient)patientComboBox.SelectedItem, doctor));
                     }
 
                     for (int i = 0; i < recommendedAppointments.Count; i++)
@@ -196,7 +196,7 @@ namespace HospitalInformationSystem.Windows
                             for (int j = 0; j < dateTimes.Count; j++)
                             {
  
-                                recommendedAppointments.Add(new Appointment(dateTimes[j], TypeOfAppointment.Pregled, doctors[i].room, (Patient)patientComboBox.SelectedItem, doctors[i]));
+                                recommendedAppointments.Add(new Appointment(dateTimes[j], TypeOfAppointment.Pregled, doctors[i].room, (Model.Patient)patientComboBox.SelectedItem, doctors[i]));
                             }
                         }
                     }

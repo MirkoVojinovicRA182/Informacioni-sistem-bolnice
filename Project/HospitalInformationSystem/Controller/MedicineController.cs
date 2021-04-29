7ec5818918e81083b268e54b4a34eaac78870391
Mirko <73440834+MirkoVojinovicRA182@users.noreply.github.com>
2021-04-29 14:11:52 +0200

    Creating MedicineController and MedicineService classes

---
+++ Project/HospitalInformationSystem/Controller/MedicineController.cs
@@ -0,0 +1,25 @@
+﻿using HospitalInformationSystem.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalInformationSystem.Controller
+{
+    class MedicineController
+    {
+        private MedicineService medicineService;
+        private static MedicineController instance = null;
+        public static MedicineController GetInstance()
+        {
+            if (instance == null)
+                instance = new MedicineController();
+            return instance;
+        }
+        private MedicineController()
+        {
+            medicineService = new MedicineService();
+        }
+    }
+}

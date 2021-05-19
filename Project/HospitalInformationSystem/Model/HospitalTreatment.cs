16ca3017d369ef07fc8c2898c444ec14200f186d
RadisaMilovcevic <milovcevic.ra193.2018@uns.ac.rs>
2021-05-19 09:25:09 +0200

    Created class HospitalTretment

---
+++ Project/HospitalInformationSystem/Model/HospitalTreatment.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+
+namespace HospitalInformationSystem.Model
+{
+    [Serializable]
+    public class HospitalTreatment
+    {
+        public HospitalTreatment(DateTime treatmentStartDate, DateTime treatmentEndDate, Room treatmentRoom)
+        {
+            this.treatmentStartDate = treatmentStartDate;
+            this.treatmentEndDate = treatmentEndDate;
+            this.treatmentRoom = treatmentRoom;
+        }
+        public DateTime treatmentStartDate { get; set; }
+        public DateTime treatmentEndDate { get; set; }
+        public Room treatmentRoom { get; set; }
+    }
+}

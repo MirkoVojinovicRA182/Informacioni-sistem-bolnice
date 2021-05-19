850b7642522b9a54f1291d59aee9d2e2f96b80d3
RadisaMilovcevic <milovcevic.ra193.2018@uns.ac.rs>
2021-05-19 09:38:24 +0200

    Added patient preview window

---
+++ Project/HospitalInformationSystem/Windows/DoctorGUI/PatientPreviewWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using HospitalInformationSystem.Controller;
+using Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HospitalInformationSystem.Windows.DoctorGUI
+{
+    /// <summary>
+    /// Interaction logic for PatientPreviewWindow.xaml
+    /// </summary>
+    public partial class PatientPreviewWindow : Window
+    {
+        private static PatientPreviewWindow instance = null;
+        private Doctor loggedInDoctor;
+        public static PatientPreviewWindow GetInstance(Doctor loggedInDoctor)
+        {
+            if (instance == null)
+                instance = new PatientPreviewWindow(loggedInDoctor);
+            return instance;
+        }
+        private PatientPreviewWindow(Doctor loggedInDoctor)
+        {
+            InitializeComponent();
+            this.loggedInDoctor = loggedInDoctor;
+            initPatientsTable();
+        }
+
+        private void initPatientsTable()
+        {
+            ObservableCollection<Patient> patients = new ObservableCollection<Patient>();
+            foreach (Patient patient in PatientController.getInstance().getPatient())
+            {
+                patients.Add(patient);
+            }
+            patientsTable.ItemsSource = patients;
+        }
+
+        private void CheckKeyPress()
+        {
+            if (Keyboard.IsKeyDown(Key.Enter))
+            {
+                ShowPatientInformationWindow window = ShowPatientInformationWindow.GetInstance((Patient)patientsTable.SelectedItem, loggedInDoctor);
+                window.Show();
+            }
+            else if ((Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)) && Keyboard.IsKeyDown(Key.T))
+            {
+                if (((Patient)patientsTable.SelectedItem).hospitalTreatment != null)
+                {
+                    EditHospitalTreatmentWindow window = EditHospitalTreatmentWindow.GetInstance((Patient)patientsTable.SelectedItem);
+                    window.Show();
+                }
+            }
+            else if (Keyboard.IsKeyDown(Key.Escape))
+            {
+                this.Close();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            instance = null;
+        }
+
+        private void patientsTable_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            CheckKeyPress();
+        }
+    }
+}

b72632462f9ff1bc13fad263d8317d35d262c3a8
RadisaMilovcevic <milovcevic.ra193.2018@uns.ac.rs>
2021-05-24 12:12:22 +0200

    Refactoring code for DoctorAddNewIngredientWindow class

--- Project/HospitalInformationSystem/Windows/DoctorGUI/DoctorAddNewIngredientWindow.xaml.cs
+++ Project/HospitalInformationSystem/Windows/DoctorGUI/DoctorAddNewIngredientWindow.xaml.cs
@@ -8,39 +8,57 @@ namespace HospitalInformationSystem.Windows.DoctorGUI
     /// </summary>
     public partial class DoctorAddNewIngredientWindow : Window
     {
-        private Medicine medicine;
-        public DoctorAddNewIngredientWindow(Medicine medicine)
+        private Medicine _medicineToAddNewIngredient;
+        private static DoctorAddNewIngredientWindow instance = null;
+        public static DoctorAddNewIngredientWindow GetInstance(Medicine medicineToAddNewIngredient)
         {
-            this.medicine = medicine;
-            InitializeComponent();
+            if (instance == null)
+                instance = new DoctorAddNewIngredientWindow(medicineToAddNewIngredient);
+            return instance;
         }
-
+        private DoctorAddNewIngredientWindow(Medicine medicineToAddNewIngredient)
+        {
+            InitializeComponent();
+            this._medicineToAddNewIngredient = medicineToAddNewIngredient;
+        }
         private void confirmButton_Click(object sender, RoutedEventArgs e)
         {
-            if(CheckInputs())
-                medicine.Ingredients.Add(new MedicineIngredient(nameTextBox.Text, double.Parse(quantityInHundredGramsTextBox.Text), int.Parse(dailyIntakeTextBox.Text)));
+            if(CheckAllInputs())
+                _medicineToAddNewIngredient.Ingredients.Add(
+                    new MedicineIngredient(nameTextBox.Text, double.Parse(quantityInHundredGramsTextBox.Text), int.Parse(dailyIntakeTextBox.Text)));
         }
-
-        private bool CheckInputs()
+        private bool CheckIngredientNameInput()
         {
-            if(nameTextBox.Text.Length < 1)
+            if (nameTextBox.Text.Length < 1)
             {
                 MessageBox.Show("Morate uneti naziv!", "Naziv", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
             }
+            return true;
+        }
+        private bool CheckQuantityInput()
+        {
             double tryParseStringToDouble = double.TryParse(quantityInHundredGramsTextBox.Text, out tryParseStringToDouble) ? tryParseStringToDouble : 0;
-            if(tryParseStringToDouble == 0)
+            if (tryParseStringToDouble == 0)
             {
                 MessageBox.Show("Greska u kolicini!", "Kolicina", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
             }
+            return true;
+        }
+        private bool CheckRdiInput()
+        {
             int tryParseStringToInt = int.TryParse(dailyIntakeTextBox.Text, out tryParseStringToInt) ? tryParseStringToInt : 0;
-            if(tryParseStringToInt == 0)
+            if (tryParseStringToInt == 0)
             {
                 MessageBox.Show("Greska u PDU!", "PDU", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
             }
             return true;
+        }
+        private bool CheckAllInputs()
+        {
+            return CheckIngredientNameInput() && CheckQuantityInput() && CheckRdiInput();
         }
     }
 }

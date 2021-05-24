e3b6c6e93868708aa9aaac07ee39f46ddc0e1558
RadisaMilovcevic <milovcevic.ra193.2018@uns.ac.rs>
2021-05-24 12:12:51 +0200

    Refactoring code for DoctorAllergensPreviewWindow class

--- Project/HospitalInformationSystem/Windows/DoctorGUI/DoctorAllergensPreviewWindow.xaml.cs
+++ Project/HospitalInformationSystem/Windows/DoctorGUI/DoctorAllergensPreviewWindow.xaml.cs
@@ -8,7 +8,14 @@ namespace HospitalInformationSystem.Windows.DoctorGUI
     /// </summary>
     public partial class DoctorAllergensPreviewWindow : Window
     {
-        public DoctorAllergensPreviewWindow(Patient patientToViewAllergens)
+        private static DoctorAllergensPreviewWindow instance = null;
+        public static DoctorAllergensPreviewWindow GetInstance(Patient patientToViewAllAllergens)
+        {
+            if (instance == null)
+                instance = new DoctorAllergensPreviewWindow(patientToViewAllAllergens);
+            return instance;
+        }
+        private DoctorAllergensPreviewWindow(Patient patientToViewAllergens)
         {
             InitializeComponent();
             allergensListBox.ItemsSource = patientToViewAllergens.Allergens;

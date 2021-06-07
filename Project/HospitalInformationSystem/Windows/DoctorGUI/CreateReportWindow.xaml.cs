81cab1d6b8d92bc8d91846b031ae9e9e2caabc16
RadisaMilovcevic <milovcevic.ra193.2018@uns.ac.rs>
2021-06-07 20:10:43 +0200

    Create view for Create report

---
+++ Project/HospitalInformationSystem/Windows/DoctorGUI/CreateReportWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace HospitalInformationSystem.Windows.DoctorGUI
+{
+    /// <summary>
+    /// Interaction logic for CreateReportWindow.xaml
+    /// </summary>
+    public partial class CreateReportWindow : Window
+    {
+        private static CreateReportWindow instance;
+        public static CreateReportWindow GetInstance()
+        {
+            if (instance == null)
+                instance = new CreateReportWindow();
+            return instance;
+        }
+        public CreateReportWindow()
+        {
+            InitializeComponent();
+            CreateReport();
+        }
+
+        private void CreateReport()
+        {
+
+        }
+    }
+}
